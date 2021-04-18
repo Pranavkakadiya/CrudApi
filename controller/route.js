8937be6304e88c75757db86fdf6773a15ec9968c
Pranav <pkakadiya274@rku.ac.in>
2021-04-18 12:25:31 +0530

    schema check 2

--- controller/route.js
+++ controller/route.js
@@ -6,104 +6,60 @@ const router = express.Router()
 router.get("/cars", async (req, res) => {
 
     const posts = await Car.find()
-
-    setTimeout(()=>{
-
-    res.send(posts)
-
-},5000)
-
+    setTimeout(() => {
+        res.send(posts)
+    }, 5000)
 })
 
 
 router.get("/cars/:id", async (req, res) => {
-
     console.log(req.params.id)
-
     const data = await Car.findOne({ _id: req.params.id })
-    
-    setTimeout(()=>{
-
-        res.send(posts)
-    
-    },5000)
-
-    console.log(data)
-
+    setTimeout(() => {
+        res.send(data)
+    }, 5000)
 })
 
 router.post("/cars", async (req, res) => {
 
     const data = new Car({
-
-        title : req.body.title,
-        content : req.body.content,
-
+        title: req.body.title,
+        content: req.body.content,
     })
-
     await data.save()
-
     res.send(data)
-
 })
-router.patch("/cars/:id", async (req, res) => {
 
+router.patch("/cars/:id", async (req, res) => {
     try {
         console.log(req.params.id)
-
         const data = await Car.findOne({ _id: req.params.id })
 
-        console.log(data)
-
-
         if (req.body.title) {
-
             data.title = req.body.title
-
         }
-
 
         if (req.body.content) {
-
             data.content = req.body.content
-
         }
+
         await data.save()
-
         res.send(data)
-
     } catch {
-
         res.status(404)
-
         res.send({ error: "Post doesn't exist!" })
-
     }
-
 })
 
-
 router.delete("/cars/:id", async (req, res) => {
-
     try {
-
-        console.log(req.params.id)
-
-
-     
         const data = await Car.deleteOne({ _id: req.params.id })
-        console.log(data)
-
-        res.status(204).send()
+        res.status(204).send(data)
 
     } catch {
-
         res.status(404)
-
-        res.send({ error: "Post doesn't exist!" })
-
+        res.send({ error: "Post doesn't exist!" + req.params.id })
     }
-
 })
 
 module.exports = router
